6e0cde582fe326d368b579cfc9b5e03c1ace22a0
sidjagarlamudi <jaglings@gmail.com>
2021-03-29 14:18:26 -0500

    updated

--- src/learning/java/Main.java
+++ src/learning/java/Main.java
@@ -1,10 +1,18 @@
 package learning.java;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!!!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please type your age:");
+
+        int userAge = input.nextInt();
+
+        System.out.println("User Age: " + userAge);
 
     }
 }

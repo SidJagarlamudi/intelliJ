5073e8b195a92f070bd173e4ac67ab4dfb8290bb
sidjagarlamudi <jaglings@gmail.com>
2021-04-05 10:08:12 -0500

    updated

--- src/learning/java/Main.java
+++ src/learning/java/Main.java
@@ -7,13 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Please type your age:");
-
-        int userAge = input.nextInt();
-
-        System.out.println("User Age: " + userAge);
-
+        System.out.println("Enter year:");
+        int year = input.nextInt();
+        int i = year % 12;
+        String response;
+        response = "";
+        switch(i){
+            case 0 : response = "monkey"; break;
+            case 1 : response = "rooster"; break;
+            case 2 : response = "dog"; break;
+            case 3 : response = "pig"; break;
+            case 4 : response = "rat"; break;
+            case 5 : response = "ox"; break;
+            case 6 : response = "tiger"; break;
+            case 7 : response = "rabbit"; break;
+            case 8 : response = "dragon"; break;
+            case 9 : response = "snake"; break;
+            case 10 : response = "horse"; break;
+            case 11 : response = "sheep"; break;
+        }
+        System.out.println("Your Chinese Zodiac sign is a " + response + "!");
     }
 }
+
+
+
 
